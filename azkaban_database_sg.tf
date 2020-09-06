6e4768dbad704e530e61045eda966b3aad187052
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-09-06 22:34:13 +0100

    Adding database

---
+++ azkaban_database_sg.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "azkaban_database" {
+  name        = "azkaban-database"
+  description = "Rules necesary for allowing access to the database"
+  vpc_id      = module.workflow_manager_vpc.vpc.id
+  tags        = merge(local.common_tags, { Name = "azkaban-database" })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "allow_azkaban_webserver_ingress_azkaban_database" {
+  description              = "Allows azkaban webserver to access azkaban database"
+  type                     = "ingress"
+  to_port                  = aws_db_instance.azkaban_database.port
+  protocol                 = "tcp"
+  from_port                = aws_db_instance.azkaban_database.port
+  security_group_id        = aws_security_group.azkaban_database.id
+  source_security_group_id = aws_security_group.azkaban_webserver.id
+}
+
+resource "aws_security_group_rule" "allow_azkaban_executor_ingress_azkaban_database" {
+  description              = "Allows azkaban executor to access azkaban database"
+  type                     = "ingress"
+  to_port                  = aws_db_instance.azkaban_database.port
+  protocol                 = "tcp"
+  from_port                = aws_db_instance.azkaban_database.port
+  security_group_id        = aws_security_group.azkaban_database.id
+  source_security_group_id = aws_security_group.azkaban_executor.id
+}
