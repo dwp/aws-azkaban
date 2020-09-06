9aa87fcd303f55d3c17d4bc6b070189fc19f339c
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-09-06 22:26:09 +0100

    Adding webserver configurations

---
+++ config/azkaban/web-server/start-web.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+script_dir=$(dirname $0)
+
+echo "Creating tables in database if they do not exist using create-all-sql.sql"
+mysql -h ${db_host} -u${db_username} -p${db_password} ${db_name} < /azkaban-db/create-all-sql-0.1.0-SNAPSHOT.sql
+
+$${script_dir}/internal/internal-start-web.sh
