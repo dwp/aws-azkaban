d3e0a3f9c8a83b82576d05f51b061e4e469df5d3
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-09-07 16:35:42 +0100

    Amending selector

--- azkaban_executor_s3.tf
+++ azkaban_executor_s3.tf
@@ -2,7 +2,7 @@ data template_file "azkaban_executor_users" {
   template = file("${path.module}/config/azkaban/exec-server/azkaban-users.xml")
   vars = {
     admin_username = jsondecode(data.aws_secretsmanager_secret_version.workflow_manager.secret_binary).azkaban_username
-    admin_password = jsondecode(data.aws_secretsmanager_secret_version.workflow_manager.secret_binary).azkaban_username
+    admin_password = jsondecode(data.aws_secretsmanager_secret_version.workflow_manager.secret_binary).azkaban_password
   }
 }
 
