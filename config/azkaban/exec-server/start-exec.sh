ed9fbe5a1e2fcf70e10d66ae1670471db76f5f15
iw-gtadam <61688288+iw-gtadam@users.noreply.github.com>
2020-10-29 08:33:28 +0000

    Removing debugging from script

--- config/azkaban/exec-server/start-exec.sh
+++ config/azkaban/exec-server/start-exec.sh
@@ -1,10 +1,6 @@
-#!/bin/bash 
- 
+#!/bin/bash
+
 script_dir=$(dirname $0)
- 
-echo "Dropping all other expired executors from table"
-mysql -h $DB_HOST -u$DB_USERNAME -p$DB_PASSWORD $DB_NAME -e \
-    "DELETE FROM executors;"
  
 echo "Creating tables in database if they do not exist using create-all-sql.sql"
 mysql -h $DB_HOST -u$DB_USERNAME -p$DB_PASSWORD $DB_NAME < /azkaban-db/create-all-sql-0.1.0-SNAPSHOT.sql
