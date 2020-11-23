c3f0408b2e473ad204517957756feced54e97bbd
iw-gtadam <61688288+iw-gtadam@users.noreply.github.com>
2020-11-23 08:44:07 +0000

    Dw 5302 log steps (#15)

--- workflow_s3.tf
+++ workflow_s3.tf
@@ -1,7 +1,3 @@
-data template_file "dummy" {
-  template = file("${path.module}/config/azkaban/dummy.sh")
-}
-
 data template_file "example_job_flow" {
   template = file("${path.module}/config/azkaban/example_job/example_job.flow")
 }
@@ -35,13 +31,6 @@ data "archive_file" "example_job" {
     content  = data.template_file.workflow_submit_step.rendered
     filename = "submit_step.sh"
   }
-}
-
-resource "aws_s3_bucket_object" "dummy" {
-  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
-  key        = "${local.name}/azkaban/dummy.sh"
-  content    = data.template_file.dummy.rendered
-  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
 }
 
 resource "aws_s3_bucket_object" "example_workflow_zip" {
