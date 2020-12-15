48b4fff9e75b9c533a21ff556c42a326b24340b1
MattToczek <toczek.matt@gmail.com>
2020-12-15 15:44:08 +0000

    removing template file and tf that uploaded it to S3

--- azkaban_executor_s3.tf
+++ azkaban_executor_s3.tf
@@ -1,11 +1,3 @@
-data template_file "azkaban_executor_users" {
-  template = file("${path.module}/config/azkaban/exec-server/azkaban-users.xml")
-  vars = {
-    admin_username = jsondecode(data.aws_secretsmanager_secret_version.workflow_manager.secret_binary).azkaban_username
-    admin_password = jsondecode(data.aws_secretsmanager_secret_version.workflow_manager.secret_binary).azkaban_password
-  }
-}
-
 data template_file "azkaban_executor_properties" {
   template = file("${path.module}/config/azkaban/exec-server/azkaban.properties")
   vars = {
@@ -37,13 +29,6 @@ data template_file "azkaban_executor_internal" {
   vars = {
     azkaban_executor_port = jsondecode(data.aws_secretsmanager_secret_version.workflow_manager.secret_binary).ports.azkaban_executor_port
   }
-}
-
-resource "aws_s3_bucket_object" "azkaban_executor_users" {
-  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
-  key        = "${local.name}/azkaban/exec-server/azkaban-users.xml"
-  content    = data.template_file.azkaban_executor_users.rendered
-  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
 }
 
 resource "aws_s3_bucket_object" "azkaban_executor_properties" {
