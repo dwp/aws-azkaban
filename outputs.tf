a0f306b1a700f0b1ea4aff6b4d6649d5a54d0ded
Dragos Costache <dragos.costache@infinityworks.com>
2020-12-16 11:48:08 +0000

    Add housekeeping concourse job for development

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+output "rds_db" {
+  value = aws_rds_cluster.azkaban_database
+}
+
+output "secrets" {
+  value = {
+    executor = aws_secretsmanager_secret.azkaban_executor_password
+    webserver = aws_secretsmanager_secret.azkaban_webserver_password
+  }
+}
+
+output "ecs_services" {
+  value = {
+    executor = aws_ecs_service.azkaban_executor
+    webserver = aws_ecs_service.azkaban_executor
+  }
+}
