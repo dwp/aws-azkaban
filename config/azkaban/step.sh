c8437bbf6aea4e833bf8bedb25b770d643dc8ec4
Thomas Caldwell <caldwellt@Thomass-MBP.lan>
2021-01-11 15:55:25 +0000

    reordering of arguments and making more readable

--- config/azkaban/step.sh
+++ config/azkaban/step.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+USERNAME=$1
+SCRIPT_NAME=$2
+SCRIPT_ARGUMENTS=${@:3}
+
 LOG_GROUP_NAME=/aws/emr/azkaban
 CONFIG_FILE=/opt/aws/amazon-cloudwatch-agent/bin/config.json
 LOG_DIR=$(ls -td -- /var/log/hadoop/steps/* | head -n 1)
@@ -26,7 +30,8 @@ sudo cat $CONFIG_FILE | jq ".logs.logs_collected.files.collect_list += [{\"file_
 sudo mv -f /tmp/config.json $CONFIG_FILE
 sudo amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:$CONFIG_FILE -s >/dev/null 2>&1
 
+sudo su -c "$SCRIPT_NAME $SCRIPT_ARGUMENTS" - $USERNAME
+
 # Synchronize external files on Batch EMR
 /home/hadoop/get_scripts.sh component/uc_repos /opt/emr/repos
 
-sudo su -c "$1 $3" - $2
