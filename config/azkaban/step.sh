340d9f58825133e8b97f833f754ba957bf992d84
PhanindraVenkata <phanindra.venkata@dwp.gsi.gov.uk>
2021-01-11 12:46:03 +0000

    Correcting the order of execution

--- config/azkaban/step.sh
+++ config/azkaban/step.sh
@@ -26,7 +26,9 @@ sudo cat $CONFIG_FILE | jq ".logs.logs_collected.files.collect_list += [{\"file_
 sudo mv -f /tmp/config.json $CONFIG_FILE
 sudo amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:$CONFIG_FILE -s >/dev/null 2>&1
 
+# Synchronize external files on Batch EMR
+sudo /home/hadoop/get_scripts.sh component/uc_repos /opt/emr/repos
+
 sudo su -c "$1 $3" - $2
 
-# Synchronize external files on Batch EMR
-/home/hadoop/get_scripts.sh component/uc_repos /opt/emr/repos
+
