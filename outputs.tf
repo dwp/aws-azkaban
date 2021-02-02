2f8eae005844dfb42ce7ca715a6aebd76ee25ac4
Mark <mark.stevens@infinityworks.com>
2021-02-02 11:16:37 +0000

    outputting workflow manager vpc
    Signed-off-by: Mark <mark.stevens@infinityworks.com>

--- outputs.tf
+++ outputs.tf
@@ -4,18 +4,22 @@ output "rds_db" {
 
 output "secrets" {
   value = {
-    executor = aws_secretsmanager_secret.azkaban_executor_password
+    executor  = aws_secretsmanager_secret.azkaban_executor_password
     webserver = aws_secretsmanager_secret.azkaban_webserver_password
   }
 }
 
 output "ecs_services" {
   value = {
-    executor = aws_ecs_service.azkaban_executor
+    executor  = aws_ecs_service.azkaban_executor
     webserver = aws_ecs_service.azkaban_executor
   }
 }
 
 output "truncate_table_lambda" {
   value = aws_lambda_function.truncate_table
+}
+
+output "workflow_manager_vpc" {
+  value = module.workflow_manager_vpc
 }
