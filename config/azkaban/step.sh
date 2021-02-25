f32450e11fd49a54cd9c3ec0cbcfa9bd4c014567
caldwelltom <76474731+caldwelltom@users.noreply.github.com>
2021-02-25 15:16:33 +0000

    Add chs-cbol-data build_table script to step script (#49)

--- config/azkaban/step.sh
+++ config/azkaban/step.sh
@@ -33,8 +33,5 @@ sudo amazon-cloudwatch-agent-ctl -a append-config -m ec2 -c file:$CONFIG_FILE -s
 
 # Synchronize external files on Batch EMR
 /home/hadoop/get_scripts.sh component/uc_repos /opt/emr/repos
-sudo chmod g+x /opt/emr/repos/chs-uc-lab/build_dataset.sh
-sudo chmod g+x /opt/emr/repos/chs-uc-lab/build_views.sh
-sudo chmod g+x /opt/emr/repos/chs-payment-timelines/scripts/build_pt.sh
 
 sudo su -c "$SCRIPT_NAME $SCRIPT_ARGUMENTS" - $USERNAME
