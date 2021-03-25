a26b700b7c25560695e77b12b57655fac0661d99
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-03-25 07:49:10 +0000

    Add sidecar to webserver, create ocnfig for sidecar
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- azkaban_webserver_ecs.tf
+++ azkaban_webserver_ecs.tf
@@ -6,7 +6,7 @@ resource "aws_ecs_task_definition" "azkaban_webserver" {
   memory                   = "4096"
   task_role_arn            = aws_iam_role.azkaban_webserver.arn
   execution_role_arn       = data.terraform_remote_state.common.outputs.ecs_task_execution_role.arn
-  container_definitions    = "[${data.template_file.azkaban_webserver_definition.rendered}]"
+  container_definitions    = "[${data.template_file.azkaban_webserver_definition.rendered}, ${data.template_file.azkaban_webserver_jmx_exporter_definition.rendered}]"
 }
 
 data "template_file" "azkaban_webserver_definition" {
@@ -35,6 +35,31 @@ data "template_file" "azkaban_webserver_definition" {
         "value" : jsondecode(data.aws_secretsmanager_secret_version.workflow_manager.secret_binary).keystore_data
       }
     ])
+  }
+}
+
+data "template_file" "azkaban_webserver_jmx_exporter_definition" {
+  template = file("${path.module}/container_definition.tpl")
+  vars = {
+    name          = "azkaban-webserver-jmx-exporter"
+    group_name    = "jmx_exporter"
+    cpu           = var.fargate_cpu
+    image_url     = format("%s:%s", data.terraform_remote_state.management.outputs.ecr_jmx_exporter_url, var.image_versions.jmx-exporter)
+    memory        = var.fargate_memory
+    user          = "root"
+    ports         = jsonencode([5556])
+    log_group     = aws_cloudwatch_log_group.workflow_manager.name
+    region        = var.region
+    config_bucket = data.terraform_remote_state.common.outputs.config_bucket.id
+
+    mount_points = jsonencode([])
+    environment_variables = jsonencode([
+      {
+        "name" : "JMX_EXPORTER_ROLE",
+        "value" : "web-server"
+      }
+    ])
+
   }
 }
 
