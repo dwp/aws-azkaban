b61ef3aaed219bd10e39dbb12ea5df422e9d846d
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-03-25 07:53:36 +0000

    dedupe s3 objects
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- azkaban_executor_s3.tf
+++ azkaban_executor_s3.tf
@@ -31,7 +31,7 @@ data template_file "azkaban_executor_internal" {
   }
 }
 
-data template_file "jmx_exporter_config" {
+data template_file "azkaban_executor_jmx_exporter_config" {
   template = file("${path.module}/config/azkaban/exec-server/jmx-exporter/config.yml")
 }
 
@@ -77,9 +77,9 @@ resource "aws_s3_bucket_object" "azkaban_executor_script" {
   kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
 }
 
-resource "aws_s3_bucket_object" "jmx_exporter_config" {
+resource "aws_s3_bucket_object" "azkaban_executor_jmx_exporter_config" {
   bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
   key        = "${local.name}/jmx_exporter/exec-server/jmx-exporter.yml"
-  content    = data.template_file.jmx_exporter_config.rendered
+  content    = data.template_file.azkaban_executor_jmx_exporter_config.rendered
   kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
 }
