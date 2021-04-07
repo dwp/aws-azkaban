f01961f05ac63250dabf5428ffa9687dc7752dd9
thomasstock-caldwell <thomas.stock-caldwell@engineering.digital.dwp.gov.uk>
2021-04-07 14:27:10 +0100

    Add copied config files to external s3 location

--- azkaban_external_executor_s3.tf
+++ azkaban_external_executor_s3.tf
@@ -25,3 +25,50 @@ resource "aws_s3_bucket_object" "azkaban_external_executor_internal" {
   content    = data.template_file.azkaban_executor_internal.rendered
   kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
 }
+
+data template_file "azkaban_external_executor_start" {
+  template = file("${path.module}/config/azkaban_external/exec-server/start-exec.sh")
+}
+
+data template_file "azkaban_external_executor_commonprivate" {
+  template = file("${path.module}/config/azkaban_external/exec-server/commonprivate.properties")
+}
+
+data template_file "azkaban_external_executor_private" {
+  template = file("${path.module}/config/azkaban_external/exec-server/private.properties")
+}
+
+resource "aws_s3_bucket_object" "azkaban_external_executor_properties" {
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "${local.name}/azkaban_external/exec-server/azkaban.properties"
+  content    = data.template_file.azkaban_external_executor_properties.rendered
+  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+}
+
+resource "aws_s3_bucket_object" "azkaban_external_executor_start" {
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "${local.name}/azkaban_external/exec-server/start-exec.sh"
+  content    = data.template_file.azkaban_external_executor_start.rendered
+  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+}
+
+resource "aws_s3_bucket_object" "azkaban_external_executor_commonprivate" {
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "${local.name}/azkaban_external/exec-server/commonprivate.properties"
+  content    = data.template_file.azkaban_external_executor_commonprivate.rendered
+  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+}
+
+resource "aws_s3_bucket_object" "azkaban_external_executor_private" {
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "${local.name}/azkaban_external/exec-server/private.properties"
+  content    = data.template_file.azkaban_external_executor_private.rendered
+  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+}
+
+resource "aws_s3_bucket_object" "azkaban_external_executor_script" {
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "${local.name}/azkaban_external/step.sh"
+  content    = file("${path.module}/config/azkaban_external/step.sh")
+  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+}
--- azkaban_external_webserver_s3.tf
+++ azkaban_external_webserver_s3.tf
@@ -14,3 +14,36 @@ resource "aws_s3_bucket_object" "azkaban_external_webserver_properties" {
   content    = data.template_file.azkaban_external_webserver_properties.rendered
   kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
 }
+
+data template_file "azkaban_external_webserver_start" {
+  template = file("${path.module}/config/azkaban_external/web-server/start-web.sh")
+}
+
+data template_file "azkaban_external_webserver_internal" {
+  template = file("${path.module}/config/azkaban_external/web-server/internal-start-web.sh")
+}
+
+data template_file "azkaban_external_webserver_jmx_exporter_config" {
+  template = file("${path.module}/config/azkaban_external/web-server/jmx-exporter/config.yml")
+}
+
+resource "aws_s3_bucket_object" "azkaban_external_webserver_start" {
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "${local.name}/azkaban_external/web-server/start-web.sh"
+  content    = data.template_file.azkaban_external_webserver_start.rendered
+  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+}
+
+resource "aws_s3_bucket_object" "azkaban_external_webserver_internal" {
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "${local.name}/azkaban_external/web-server/internal-start-web.sh"
+  content    = data.template_file.azkaban_external_webserver_internal.rendered
+  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+}
+
+resource "aws_s3_bucket_object" "azkaban_external_webserver_jmx_exporter_config" {
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "${local.name}/jmx_exporter/web-server/jmx-exporter.yml"
+  content    = data.template_file.azkaban_external_webserver_jmx_exporter_config.rendered
+  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+}
