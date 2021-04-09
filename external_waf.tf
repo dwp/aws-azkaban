07af7b5c118a202c99b49f4bf7359ac8c2e7b6b2
thomasstock-caldwell <thomas.stock-caldwell@engineering.digital.dwp.gov.uk>
2021-04-09 14:32:53 +0100

    Add azkaban external waf

---
+++ external_waf.tf
@@ -0,0 +1,15 @@
+module "waf" {
+  source = "dwp/waf/aws"
+
+  name                  = local.external_name
+  s3_log_bucket         = data.terraform_remote_state.security-tools.outputs.logstore_bucket.arn
+  s3_log_prefix         = "waf/${local.external_name}"
+  whitelist_cidr_blocks = var.whitelist_cidr_blocks
+
+  tags = merge(local.common_tags, { Name = "${local.external_name}-azkaban-external-waf" })
+}
+
+resource "aws_wafregional_web_acl_association" "lb" {
+  resource_arn = aws_lb.azkaban_external.arn
+  web_acl_id   = module.waf.wafregional_web_acl_id
+}
