880f6f4840c752b9d187640391c466eba2a63c48
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-06-03 14:13:37 +0100

    FMT
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- local.tf
+++ local.tf
@@ -1,6 +1,6 @@
 locals {
   azkaban_external_cognito_secret = {
     name = data.terraform_remote_state.dataworks_secrets.outputs.azkaban_external_mgmt.name
-    arn = data.terraform_remote_state.dataworks_secrets.outputs.azkaban_external_mgmt.arn
+    arn  = data.terraform_remote_state.dataworks_secrets.outputs.azkaban_external_mgmt.arn
   }
 }
