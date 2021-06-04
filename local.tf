e2279bcd0394226b51a319497095b8c9dbd70149
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-06-04 15:02:33 +0100

    Add monitoring for web service
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- local.tf
+++ local.tf
@@ -9,7 +9,7 @@ locals {
 
   monitoring_topic_arn = data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn
 
-  external_alert_on_running_tasks_less_than_desired = {
+  azkaban_external_alert_on_running_tasks_less_than_desired = {
     development = true
     qa          = false
     integration = false
@@ -17,6 +17,17 @@ locals {
     production  = true
   }
 
-  external_running_tasks_less_than_desired = "Azkaban External - Running tasks less than desired for more than 5 minutes"
+  azkaban_alert_on_running_tasks_less_than_desired = {
+    development = true
+    qa          = false
+    integration = false
+    preprod     = false
+    production  = true
+  }
+
+  azkaban_external_running_tasks_less_than_desired = "Azkaban External Executor - Running tasks less than desired for more than 5 minutes"
+  azkaban_external_web_running_tasks_less_than_desired = "Azkaban External Web - Running tasks less than desired for more than 5 minutes"
+
+  azkaban_running_tasks_less_than_desired = "Azkaban External - Running tasks less than desired for more than 5 minutes"
 
 }
