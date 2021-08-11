8e4cd712d8afc6f2f1b22f7300439f919091c382
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-08-11 14:16:24 +0100

    Double Fargate task CPU
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- azkaban_webserver_ecs.tf
+++ azkaban_webserver_ecs.tf
@@ -2,7 +2,7 @@ resource "aws_ecs_task_definition" "azkaban_webserver" {
   family                   = "azkaban-webserver"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
+  cpu                      = "1024"
   memory                   = "4096"
   task_role_arn            = aws_iam_role.azkaban_webserver.arn
   execution_role_arn       = data.terraform_remote_state.common.outputs.ecs_task_execution_role.arn
