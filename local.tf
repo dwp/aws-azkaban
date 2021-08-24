6b4cc8acfa9a060a3620f811da92ab37aff7ee45
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-08-24 09:24:29 +0100

    Update locals
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- local.tf
+++ local.tf
@@ -1,6 +1,6 @@
 locals {
   azkaban_external_ecs_cluster = data.terraform_remote_state.common.outputs.ecs_cluster_main
-  azkaban_user_ecs_cluster = data.terraform_remote_state.common.outputs.ecs_cluster_main
+  azkaban_user_ecs_cluster     = data.terraform_remote_state.common.outputs.ecs_cluster_main
 
   azkaban_zip_uploader_log_level = {
     development = "DEBUG"
@@ -30,9 +30,9 @@ locals {
 
   azkaban_external_executor_running_tasks_less_than_desired = "Azkaban External Executor - Running tasks less than desired for more than 5 minutes"
   azkaban_external_web_running_tasks_less_than_desired      = "Azkaban External Web - Running tasks less than desired for more than 5 minutes"
-  azkaban_external_web_unhealthy_hosts      = "Azkaban External Web - Number of healthy nodes don't match running tasks for more than 5 minutes"
-  azkaban_external_web_zero_unhealthy_hosts = "Azkaban External Web - No healthy hosts but tasks are running"
-  azkaban_external_web_5xx_errors = "Azkaban External Web - HTTP 500 errors"
+  azkaban_external_web_unhealthy_hosts                      = "Azkaban External Web - Number of healthy nodes don't match running tasks for more than 5 minutes"
+  azkaban_external_web_zero_unhealthy_hosts                 = "Azkaban External Web - No healthy hosts but tasks are running"
+  azkaban_external_web_5xx_errors                           = "Azkaban External Web - HTTP 500 errors"
 
   azkaban_user_alert_on_running_tasks_less_than_desired = {
     development = true
@@ -52,9 +52,9 @@ locals {
 
   azkaban_user_executor_running_tasks_less_than_desired = "Azkaban User Executor - Running tasks less than desired for more than 5 minutes"
   azkaban_user_web_running_tasks_less_than_desired      = "Azkaban User Web - Running tasks less than desired for more than 5 minutes"
-  azkaban_user_web_unhealthy_hosts      = "Azkaban User Web - Number of healthy nodes don't match running tasks for more than 5 minutes"
-  azkaban_user_web_zero_unhealthy_hosts = "Azkaban User Web - No healthy hosts but tasks are running"
-  azkaban_user_web_5xx_errors = "Azkaban User Web - HTTP 500 errors"
+  azkaban_user_web_unhealthy_hosts                      = "Azkaban User Web - Number of healthy nodes don't match running tasks for more than 5 minutes"
+  azkaban_user_web_zero_unhealthy_hosts                 = "Azkaban User Web - No healthy hosts but tasks are running"
+  azkaban_user_web_5xx_errors                           = "Azkaban User Web - HTTP 500 errors"
 
   desired_executor_count = {
     development = 3
