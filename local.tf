ad1b26059cadc33a13bcee949ff1b2c0b0e4b32a
Connor Avery <55276424+connoravo@users.noreply.github.com>
2021-08-24 16:15:16 +0100

    Bump webserver to v0.0.139 (#128)
    * Monitoring canary flow alerts
    
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
    
    * Bump web server version to 0.0.139
    
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
    
    Co-authored-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- local.tf
+++ local.tf
@@ -31,11 +31,20 @@ locals {
     production  = true
   }
 
+  azkaban_external_alert_monitoring_canary = {
+    development = true
+    qa          = false
+    integration = false
+    preprod     = false
+    production  = true
+  }
+
   azkaban_external_executor_running_tasks_less_than_desired = "Azkaban External Executor - Running tasks less than desired for more than 5 minutes"
   azkaban_external_web_running_tasks_less_than_desired      = "Azkaban External Web - Running tasks less than desired for more than 5 minutes"
   azkaban_external_web_unhealthy_hosts                      = "Azkaban External Web - Number of healthy nodes don't match running tasks for more than 5 minutes"
   azkaban_external_web_zero_unhealthy_hosts                 = "Azkaban External Web - No healthy hosts but tasks are running"
   azkaban_external_web_5xx_errors                           = "Azkaban External Web - HTTP 500 errors"
+  azkaban_external_monitoring_canary_success                = "Azkaban External - Monitoring canary task hasn't ran or failed to succeed in 15 minutes"
 
   azkaban_user_alert_on_running_tasks_less_than_desired = {
     development = true
