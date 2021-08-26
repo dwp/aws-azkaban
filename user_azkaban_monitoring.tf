457c2261cc9f1d0f5eb3e7ae04b066e350616a18
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-08-26 10:09:10 +0100

    Increase time for recurrent 500 errors before alarm
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- user_azkaban_monitoring.tf
+++ user_azkaban_monitoring.tf
@@ -246,10 +246,11 @@ resource "aws_cloudwatch_metric_alarm" "user_web_healthy_hosts_zero_but_running_
 resource "aws_cloudwatch_metric_alarm" "user_web_5xx_errors" {
   alarm_name          = local.azkaban_user_web_5xx_errors
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
+  threshold           = "1"
+  evaluation_periods  = "5"
   metric_name         = "HTTPCode_ELB_5XX_Count"
   namespace           = "AWS/ApplicationELB"
-  period              = "300"
+  period              = "60"
   statistic           = "Sum"
 
   dimensions = {
