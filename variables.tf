3f68b9b7a2f0d5fea729cab256e6d4ed8e24cd7d
Sumit Bharadia <36968381+03ce007@users.noreply.github.com>
2023-01-12 14:09:09 +0000

    bump azkaban-executor image in lower env (#165)

--- variables.tf
+++ variables.tf
@@ -103,9 +103,9 @@ variable "executor_image_version" {
   description = "pinned Azkaban Executor image versions to use"
   default = {
     development = "latest"
-    qa          = "0.0.165"
-    integration = "0.0.165"
-    preprod     = "0.0.165"
+    qa          = "0.0.166"
+    integration = "0.0.166"
+    preprod     = "0.0.166"
     production  = "0.0.164"
   }
 }
@@ -125,9 +125,9 @@ variable "external_executor_image_version" {
   description = "pinned Azkaban Executor image versions to use"
   default = {
     development = "latest"
-    qa          = "0.0.165"
-    integration = "0.0.165"
-    preprod     = "0.0.165"
+    qa          = "0.0.166"
+    integration = "0.0.166"
+    preprod     = "0.0.166"
     production  = "0.0.164"
   }
 }
